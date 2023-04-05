67a86d2ffa856660e954b70512d876ea11182a6b
Olar-Alex-Student <117835182+Olar-Alex-Student@users.noreply.github.com>
2023-04-05 21:18:19 +0300

    Realizat aproximativ toate cerintele pentru modelul de test

---
+++ FiguraGeometrica/Figura.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiguraGeometrica
+{
+    public class Figura
+    {
+        public int[] Laturi;
+
+        public Figura() 
+        {
+            Laturi = new int[] {};
+        }
+
+        public Figura(int NrLaturi)
+        {
+            Laturi = new int[NrLaturi];
+        }
+
+        public void TipFigura()
+        {
+            if( Laturi.Count() == 0 || Laturi.Count() == 1 || Laturi.Count() == 2)
+            {
+                Console.WriteLine("Inexistenta");
+            }
+            if (Laturi.Count() == 3)
+            {
+                Console.WriteLine("Triunghi");
+            }
+            if (Laturi.Count() == 4)
+            {
+                Console.WriteLine("Patrulater");
+            }
+            if (Laturi.Count() == 5)
+            {
+                Console.WriteLine("Pentagon");
+            }
+            if (Laturi.Count() == 6)
+            {
+                Console.WriteLine("Hexagon");
+            }
+            if (Laturi.Count() == 7)
+            {
+                Console.WriteLine("Heptagon");
+            }
+            if (Laturi.Count() == 8)
+            {
+                Console.WriteLine("Octagon");
+            }
+        }
+
+        public int NrLaturi()
+        {
+            int Total = Laturi.Count();
+            return Total;
+        }
+
+        public void LungimiLaturi()
+        {
+            for(int i = 0; i < Laturi.Count(); i++)
+            {
+                Console.WriteLine("Introdu val pt. latura {0}", i+1);
+                string text = Console.ReadLine();
+                int valoare = Int32.Parse(text);
+                Laturi[i] = valoare;
+            }
+        }
+
+        public void Latura1()
+        {
+            if (Laturi.Count() == 0)
+            {
+                Console.WriteLine("Latura 1 a figurii este: {0}",0);
+            }
+            else
+            {
+                Console.WriteLine("Latura 1 a figurii este: {0}", Laturi[0]);
+            }
+        }
+
+        public int Perimetru()
+        {
+            int suma = Enumerable.Sum(Laturi);
+            return suma;
+        }
+    }
+}
---
+++ Model-Test-1-PIU/Program.cs
@@ -0,0 +1,32 @@
+﻿using FiguraGeometrica;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model_Test_1_PIU
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Figura figura1 = new Figura();
+            Figura figura2 = new Figura(5);
+            figura1.TipFigura();
+            figura2.TipFigura();
+            Console.WriteLine("Intordu Laturile pentru Figura 1:");
+            figura1.LungimiLaturi();
+            Console.WriteLine("Intordu Laturile pentru Figura 2:");
+            figura2.LungimiLaturi();
+            Console.WriteLine("Figura 1 nr de laturi: {0}", figura1.NrLaturi());
+            Console.WriteLine("Figura 2 nr de laturi: {0}", figura2.NrLaturi());
+            figura1.Latura1();
+            figura2.Latura1();
+            Console.WriteLine("Figura 1 are perimetrul: {0}", figura1.Perimetru());
+            Console.WriteLine("Figura 2 are perimetrul: {0}", figura2.Perimetru());
+            Console.ReadKey();
+        }
+    }
+}
